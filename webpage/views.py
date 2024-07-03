9e406790b3866593190149599976c6ca74ec7fa2
mmkunlanat <stu6612732138@sskru.ac.th>
2024-07-03 21:48:56 +0700

    Django Templates

---
+++ webpage/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+def indexPage(request):
+    return render(request, 'index.html')
+
+
+
+def aboutUs(request):
+    return HttpResponse("เกี่ยวกับฉัน")
+
+
+def contactUs(request):
+    return HttpResponse("ติดต่อฉัน")
