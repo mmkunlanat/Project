43214df5a63e9f498beabd3adf8b49ffc88a9892
mmkunlanat <stu6612732138@sskru.ac.th>
2024-07-30 11:49:03 +0700

    update1

--- webpage/urls.py
+++ webpage/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.indexPage),
-    path("about/", views.aboutPage),
-    path("contact/", views.contactPage),
+    path("", views.indexPage, name="index"),
+    path("about/", views.aboutPage, name="about"),
+    path("contact/", views.contactPage, name="contact"),
 ]
