fd6f85fe5946e1e9a1d58d1c2758d0a784b6b9eb
mmkunlanat <stu6612732138@sskru.ac.th>
2024-08-13 22:35:08 +0700

    add page & config

--- webpage/urls.py
+++ webpage/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("", views.indexPage, name="index"),
     path("about/", views.aboutPage, name="about"),
     path("contact/", views.contactPage, name="contact"),
+    path("table/", views.table_Page, name="table-page"),
+    path("card/", views.card_Page, name="card-page"),
+    path("card_color/", views.cardColorPage, name="color"),
 ]
--- webpage/views.py
+++ webpage/views.py
@@ -6,10 +6,36 @@ def indexPage(request):
     return render(request, 'index.html')
 
 
-
 def aboutPage(request):
     return render(request, 'about.html')
 
 
 def contactPage(request):
     return render(request, 'contact.html')
+
+
+def table_Page(request):
+    context = {}
+    lt = list(range(0, 100))
+    context["list"] = lt
+
+    return render(request, 'table_page.html', context)
+
+
+def card_Page(request):
+    cardlist = {}
+    lt = list(range(0, 100))
+    cardlist["list"] = lt
+
+    return render(request, 'card_page.html', cardlist)
+
+
+def cardColorPage(request):
+    context = {
+        'color': '',
+    }
+
+    if request.method == "GET":
+        context['color'] = request.GET.get('color')
+
+    return render(request, 'card_color.html', context)
